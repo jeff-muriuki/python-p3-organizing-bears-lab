3870e830c0469e2f68f7ad90f2f1c782d6e9feab
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-05-27 16:56:03 +0300

    bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'brown', 'aggressive', 1);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES ('Rowdy', 2, 'F', 'blue', 'mean', 1);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES ('Tabitha', 3, 'M', 'green', 'shy', 1);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES ('Sergeant Brown', 4,'F', 'brown', 'ferocious', 1);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'brown', 'fat', 1);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'white', 'grizzly', 1);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'grey', 'malicious', 1);
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES (NULL, 8, 'M', 'black', 'mean', 0); 
